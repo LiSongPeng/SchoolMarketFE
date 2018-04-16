9cb703640f717134d3d63f1751f49f3229e2f15d
LiSongPeng <1442364096@qq.com>
2018-04-16 16:27:26 +0800

    校园市场前端工程

---
+++ app.js
@@ -0,0 +1,31 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use("/404", function (req, res) {
+    var options = {
+        root: __dirname + '/public/',
+        dotfiles: 'deny',
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+    };
+    res.sendFile("404.html", options);
+})
+app.use(function (req, res, next) {
+    res.redirect("/404");
+});
+module.exports = app;
