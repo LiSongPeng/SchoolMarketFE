695a8fa78f4b125d94c8914153d761b5abe67ac0
LiSongPeng <1442364096@qq.com>
2018-04-18 22:04:34 +0800

    处理通知功能

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,42 @@
+var currUser = window.sessionStorage.getItem(CURRENT_USER);
+/*if (!currUser) {
+    window.location.href = "login.html";
+}*/
 var index = angular.module("index", []);
 index.controller("topBarController", ["$scope", "$http", function ($scope, $http) {
     $scope.searchProduct = function () {
-        alert("开始搜索:" + $scope.searchKeyWord);
+        window.location.href = "search.html?keyWord=" + $scope.searchKeyWord;
     };
 }]);
 index.controller("sideBarController", ["$scope", "$http", function ($scope, $http) {
 }]);
 index.controller("notificationController", ["$scope", "$http", function ($scope, $http) {
+    $scope.refresh = function () {
+        $http({
+            method: "GET",
+            url: BASE_URL + "/user/login.do",
+        }).then(function successCallback(response) {
+            console.log(response.data);
+            if (response.flag == FLAG_SUCCESS) {
+                var currentUser = JSON.stringify(response.data);
+                sessionStorage.setItem(CURRENT_USER, currentUser);
+                if ($scope.isRemember) {
+                    Cookie.set(COOKIE_USERNAME, $scope.phoneOrEmail + ":" + $scope.password);
+                }
+                location.href = "index.html";
+            } else if (response.flag = FLAG_FAIL) {
+                swal(response.message);
+            }
+
+        }, function errorCallback(response) {
+            swal("登录失败，系统出现错误，请稍后重试！")
+        });
+    };
+    $scope.removeAll = function () {
+
+    };
+    $scope.removeItem = function () {
+
+    };
+    $scope.refresh();
 }]);
